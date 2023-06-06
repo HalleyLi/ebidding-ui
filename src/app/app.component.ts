2ae10d13c5f7c6b1a3dd216f674c886fcc250de9
HaileyLi <1638343987@qq.com>
2023-06-05 23:18:59 -0400

    feat: include ant design library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    HttpClientModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+  ],
 })
 export class AppComponent {
+  isCollapsed = false;
   title = 'ebidding-ui';
 }
