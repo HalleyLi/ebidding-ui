1159b0e950c33faf384cfc2f746e04c26d757a6a
HaileyLi <1638343987@qq.com>
2023-06-25 03:03:05 -0400

    refactor: charts code

--- src/app/pages/sales-portal/bwic-popular/bwic-popular.component.ts
+++ src/app/pages/sales-portal/bwic-popular/bwic-popular.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NGX_ECHARTS_CONFIG, NgxEchartsModule } from 'ngx-echarts';
 import { SalesPortalService } from '@app/core/services/sales-portal.service';
 import { BwicPopularItem } from '@app/models/bwic/bwic-popular';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { EChartsOption } from 'echarts';
 
 @Component({
@@ -12,8 +15,9 @@ import { EChartsOption } from 'echarts';
     NgxEchartsModule
   ],
   template: `
-  <div echarts style="height: 600px;" [options]="options" ></div>
+  <div echarts style="height: 600px;" [options]="options | async"></div>
 `,
+changeDetection: ChangeDetectionStrategy.OnPush,
 providers: [
   {
     provide: NGX_ECHARTS_CONFIG,
@@ -23,48 +27,47 @@ providers: [
 })
 export class BwicPopularComponent implements OnInit {
   constructor(private salesService: SalesPortalService) {}
-  options!: EChartsOption;
+  options!: Observable<EChartsOption>;
+  destroyRef = inject(DestroyRef);
+  
   ngOnInit(): void {
-    this.salesService.getAllBwicPopularList().subscribe((data: BwicPopularItem[]) => {
-      if (data) {
-        this.options = this.getOption({ bwicPopularList: data });
-      }
-    });
+    this.options = this.salesService.getAllBwicPopularList()
+    .pipe(
+      takeUntilDestroyed(this.destroyRef),
+      map(data =>({
+        title: {
+          text: 'Bidding Popular Rank',
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow',
+          },
+        },
+        legend: {},
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true,
+        },
+        xAxis: {
+          type: 'value',
+          boundaryGap: [0, 0.01],
+        },
+        yAxis: {
+          type: 'category',
+          data: data.map((item: BwicPopularItem) => item.cusip),
+        },
+        series: [
+          {
+            name: 'bidding numbers',
+            type: 'bar',
+            data: data.map((item: BwicPopularItem) => item.numberOfBids).sort(),
+          },
+        ],
+      }))
+    );
   }
 
-  getOption({ bwicPopularList }: { bwicPopularList: BwicPopularItem[]; }): EChartsOption {
-    return {
-      title: {
-        text: 'Bidding Popular Rank',
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'shadow',
-        },
-      },
-      legend: {},
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true,
-      },
-      xAxis: {
-        type: 'value',
-        boundaryGap: [0, 0.01],
-      },
-      yAxis: {
-        type: 'category',
-        data: bwicPopularList.map((item: BwicPopularItem) => item.cusip),
-      },
-      series: [
-        {
-          name: 'bidding numbers',
-          type: 'bar',
-          data: bwicPopularList.map((item: BwicPopularItem) => item.numberOfBids).sort(),
-        },
-      ],
-    };
-  }
 }
